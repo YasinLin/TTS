c804eab3e8f16cc0ae8ba8a94f86cbaacdd6fadc
yasin <l1y0l20@qq.com>
2023-12-15 22:26:24 +0800

    modify

--- TTS/tts/layers/xtts/trainer/dataset.py
+++ TTS/tts/layers/xtts/trainer/dataset.py
@@ -115,6 +115,7 @@ class XTTSDataset(torch.utils.data.Dataset):
         return tokens
 
     def load_item(self, sample):
+        print(sample)
         text = str(sample["text"])
         if "phonemes" in sample and sample["phonemes"] == "1":
             tseq = self.get_phonemes(text, sample["language"])
